ef3817d5f57bdd97376de03395f2a61450d957e9
Andrew Chaa <andrew.yh.chaa@gmail.com>
2017-08-23 18:11:41 +0100

    silent install

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -18,7 +18,7 @@ $packageArgs = @{
   checksum64    = ''
   checksumType64= 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/install /quiet /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
